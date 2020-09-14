b04f58580697a4ee3a6a9ee3284d985f2822f3b5
Alex Borodin <aiborodin@sportmaster.ru>
2020-09-14 15:42:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="App.vue__container">
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+    name: 'App.vue',
+
+    components: {},
+
+    props: {},
+
+    data() {
+      return {};
+    },
+
+    computed: {},
+
+    methods: {},
+
+  };
+</script>
+
+<style scoped>
+
+</style>
